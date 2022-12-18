e0ea9ec6203166fa8bf6b95ed9e4f0c86c61b318
Ran Kirlian <rankirlian@gmail.com>
2022-12-18 20:04:28 +0100

    Add loop examples

---
+++ Part 2 - Loops/for.py
@@ -0,0 +1,6 @@
+text = input ('Enter string: ')
+cont= 0
+for letter in text:
+  cont += 1
+else: # execute this when loop finishes
+  print ('string length:', cont)
---
+++ Part 2 - Loops/while.py
@@ -0,0 +1,9 @@
+cont = 0
+maximo = int(input ('Max limit: '))
+
+while cont <= maximo:
+  print (cont)
+  cont+= 1
+else: 
+    print ('end of cycle')
+
