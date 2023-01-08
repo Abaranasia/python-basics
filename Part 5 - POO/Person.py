7de0bb73fc8b082c23ccfc531cea47df56d8d8a5
Ran Kirlian <rankirlian@gmail.com>
2023-01-08 12:27:23 +0100

    Update Person class to add extra methods and examples

---
+++ Part 5 - POO/Person.py
@@ -0,0 +1,28 @@
+class Person:
+
+  def __init__(self, name, email, age): # constructor (dunder method)
+      self._name= name # We add underscore to indicate that this property should not be directly modified
+      self._email= email
+      self._age= age
+
+  @property
+  def name(self): # property getter
+    return self._name
+  
+  @name.setter 
+  def name(self, name): # property setter
+    self._name = name
+
+  def display_details(self):
+    print (f'Object details: {self._name} {self._email} {self._age}')
+
+person1 = Person('John', '@john', 39)
+person2 = Person('Ann', '@ann', 27)
+
+person1.display_details()
+person2.display_details()
+
+# using getter and setter to read and set properties of the object
+
+person1.name= "Edward"
+print (person1.name)
