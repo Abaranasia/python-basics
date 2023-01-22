343bddc4fb8044043ee77d189b59672ad2380488
Ran Kirlian <rankirlian@gmail.com>
2023-01-22 13:23:01 +0100

    Update Product and Order classes to remove exmaple objects and method calls

--- Part 5 - POO/Products_and_orders/Product.py
+++ Part 5 - POO/Products_and_orders/Product.py
@@ -33,11 +33,3 @@ class Product:
 
     def __str__(self):
       return (f'Product Id: {self._id_product}, Name: {self._name}, Price: {self._price}')
-
-if __name__ == '__main__':
-  product1 = Product('Laptop Asus MK100F', 1650.00)
-  print (product1)
-  product2 = Product('Laptop Asus MXP240F', 1800.00)
-  print (product2)
-  product3 = Product('Optical mouse Logitech MX100', 100.00)
-  print (product3)
