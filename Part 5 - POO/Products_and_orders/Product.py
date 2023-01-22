057ddde4a66888e2af3c86953d1cb0efb0efc519
Ran Kirlian <rankirlian@gmail.com>
2023-01-22 12:42:09 +0100

    Add a unittest simple example

---
+++ Part 5 - POO/Products_and_orders/Product.py
@@ -0,0 +1,41 @@
+class Product:
+    products_count = 0
+
+    def __init__(self, name, price):
+      Product.products_count += 1
+      self._id_product = Product.products_count
+      self._name = name
+      self._price = price
+
+    @property
+    def id_product(self): # property getter
+      return self._id_product
+    
+    @id_product.setter 
+    def id_product(self, id_product): # property setter
+      self._id_product = id_product
+    
+    @property
+    def name(self): 
+      return self._name
+    
+    @name.setter 
+    def name(self, name): 
+      self._name = name
+
+    @property
+    def price(self):
+      return self._price
+    
+    @price.setter 
+    def price(self, price): 
+      self._price = price
+
+    def __str__(self):
+      return (f'Product Id: {self._id_product}, Name: {self._name}, Price: {self._price}')
+
+if __name__ == '__main__':
+  product1 = Product('Laptop Asus MX100F', 1650.00)
+  print (product1)
+  product2 = Product('Laptop Asus MX240F', 1800.00)
+  print (product2)
