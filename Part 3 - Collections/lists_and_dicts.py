e547c9e70edee31b2eebe7c84d02277311e55f27
Ran Kirlian <rankirlian@gmail.com>
2023-11-19 14:14:12 +0100

    Update formatting

--- Part 3 - Collections/lists_and_dicts.py
+++ Part 3 - Collections/lists_and_dicts.py
@@ -1,66 +1,57 @@
-colors = ['white', 'grey', 'cyan', 'green', 'purple', 'black'] 
-print (colors)
+colors = ["white", "grey", "cyan", "green", "purple", "black"]
+print(colors)
+
 
 def insert_after(new_color, existing_color):
-  # This function will insert a given color (newcolor) after an existing color
+    # This function will insert a given color (newcolor) after an existing color
 
-  start_position = colors.index(existing_color)
-  colors.insert(start_position+1, new_color)
-  print (colors)
+    start_position = colors.index(existing_color)
+    colors.insert(start_position + 1, new_color)
+    print(colors)
 
-insert_after('yellow', 'cyan') #Should insert yellow between cyan and green
+
+insert_after("yellow", "cyan")  # Should insert yellow between cyan and green
 
 #####################################################################
 
 items = [
-  {
-    "id": 11,
-    "name": "aaa",
-    "next":22
-  },
-  {
-    "id": 22,
-    "name": "bbb",
-    "next":33
-  },
-  {
-    "id": 33,
-    "name": "zzz",
-    "next": 999
-  }
+    {"id": 11, "name": "aaa", "next": 22},
+    {"id": 22, "name": "bbb", "next": 33},
+    {"id": 33, "name": "zzz", "next": 999},
 ]
 
-new_item = {
-    "id": 23,
-    "name": "ccc",
-    "next": 0
-}
+new_item = {"id": 23, "name": "ccc", "next": 0}
+
 
 def print_object(item_id):
-  for item in items:
-   if item['id'] == item_id:
-      print (item)
+    for item in items:
+        if item["id"] == item_id:
+            print(item)
 
 
 print_object(11)
 
 #####################################################################
 
+
 def insert_inbetween(new_item, previous_item_id):
-  # This function will insert a given item inbetween two exisitng items and reconnect them
-  print ("Initial: ", items)
-  previous_item = next(filter(lambda x: x['id'] == previous_item_id, items), None)
-  # this code returns the previous item if found by its id or None if not
+    # This function will insert a given item inbetween two exisitng items and reconnect them
+    print("Initial: ", items)
+    previous_item = next(filter(lambda x: x["id"] == previous_item_id, items), None)
+    # this code returns the previous item if found by its id or None if not
 
-  if (previous_item is not None):
-    previous_item_position= items.index(previous_item) 
-    next_reference= previous_item['next'] # We need to get the reference to the next item
+    if previous_item is not None:
+        previous_item_position = items.index(previous_item)
+        next_reference = previous_item[
+            "next"
+        ]  # We need to get the reference to the next item
 
-    previous_item['next']=new_item['id']
-    new_item['next']= next_reference
+        previous_item["next"] = new_item["id"]
+        new_item["next"] = next_reference
 
-    items.insert(previous_item_position+1, new_item)
+        items.insert(previous_item_position + 1, new_item)
 
-  print ('after: ',items)
+    print("after: ", items)
 
-insert_inbetween(new_item, 122) #Should insert the new item between 22 and 33
+
+insert_inbetween(new_item, 22)  # Should insert the new item between 22 and 33
--- Part 6 - Files/file_class/File_handler.py
+++ Part 6 - Files/file_class/File_handler.py
@@ -1,48 +1,44 @@
 class File_handler:
+    folder_route = "Part 6 - Files/file_class/"
+    file_type = ""
+    text = ""
 
-  file_type=''
-  text=''
-  
-  def __init__(self, file_type):
-    self.file_type= file_type
-  
-  def create_route (self):
-    return 'Part 6 - Files/file_class/'+ self.file_type +'/'+ self.file_type+'.txt'
+    def __init__(self, file_type):
+        self.file_type = file_type
 
+    def create_route(self):
+        return self.folder_route + self.file_type + "/" + self.file_type + ".txt"
 
+    def writer(self, text):
+        route = self.create_route()
+        try:
+            file = open(route, "a", encoding="utf8")
+            file.write(f"{text} \n")
 
-  def writer(self, text):
-    route= self.create_route()
-    try: 
-      file = open (route, 'a', encoding= 'utf8')
-      file.write (f'{text} \n')
+        except Exception as e:
+            print(e)
 
-    except Exception as e: 
-      print (e)
+        finally:
+            print("file saved!")
+            file.close()
 
-    finally:
-      print ('file saved!')
-      file.close()
+    def reader(self):
+        route = self.create_route()
+        try:
+            file = open(route, "r", encoding="utf8")
+            print(f"File content: \n{file.read()}")
+        except Exception as e:
+            print(e)
 
+        finally:
+            file.close()
 
+    def delete(self):
+        import os
 
-  def reader(self):
-    route= self.create_route()
-    try:
-      file = open (route, 'r', encoding= 'utf8')
-      print (f'File content: \n{file.read()}')
-    except Exception as e: 
-      print (e)
+        route = self.create_route()
 
-    finally:
-      file.close()
-
-
-  def delete(self):
-    import os
-    route= self.create_route()
-
-    if os.path.exists(route):
-      os.remove(route) 
-    else:
-      print("The file does not exist") 
+        if os.path.exists(route):
+            os.remove(route)
+        else:
+            print("The file does not exist")
